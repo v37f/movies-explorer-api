22fb488049d33dd2b3fd46f3762c2bf28bc8ddf3
Vladimir Frolov <frolov_vo@inbox.ru>
2023-04-12 21:58:55 +0300

    feat: add users and movies routes

---
+++ routes/movies.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+// const { validateMovieInfo, validateMovieId } = require('../middlewares/validation');
+
+const {
+  getMovies,
+  createMovie,
+  deleteMovie,
+} = require('../controllers/movies');
+
+router.get('/', getMovies);
+router.post('/', /* validateMovieInfo, */ createMovie);
+
+router.delete('/:cardId', /* validateMovieId, */ deleteMovie);
