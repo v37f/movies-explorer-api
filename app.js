4ec2e48bc76a3cf65d8816ae6744b6c017a3f375
Vladimir Frolov <frolov_vo@inbox.ru>
2023-04-12 23:44:55 +0300

    feat: add centralized error handler

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-
+const { errorHandler } = require('./middlewares/error-handler');
 const { PORT, DB_ADDRESS, ORIGIN } = require('./config');
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_ADDRESS);
