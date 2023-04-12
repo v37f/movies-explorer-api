a2685e4104573725c8d47a801780fd6ae9962f19
Vladimir Frolov <frolov_vo@inbox.ru>
2023-04-12 21:21:41 +0300

    feat: add user and movie models

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { PORT, DB_ADDRESS, ORIGIN } = require('./config');
 
 const app = express();
 
-app.listen(3000, () => {
+mongoose.set('strictQuery', true);
+mongoose.connect(DB_ADDRESS);
+
+app.listen(PORT, () => {
   console.log('server listen');
 });
---
+++ config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const { NODE_ENV } = process.env;
+const { JWT_SECRET_PRODUCTION } = process.env;
+const { PORT = '3000' } = process.env;
+const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { ORIGIN = 'localhost:3001' } = process.env;
+
+const JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET_PRODUCTION : 'dev-secret';
+
+module.exports = {
+  JWT_SECRET,
+  PORT,
+  DB_ADDRESS,
+  ORIGIN,
+};
