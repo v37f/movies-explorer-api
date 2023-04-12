8daf3d0eca621aa4fefa6010d4c5e0ef97a719e3
Vladimir Frolov <frolov_vo@inbox.ru>
2023-04-12 23:35:21 +0300

    feat: add authentication

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
-// const { createUser, login } = require('../controllers/users');
+const { createUser, login } = require('../controllers/users');
 // const { validateAuthInfo } = require('../middlewares/validation');
-// const { auth } = require('../middlewares/auth');
+const { auth } = require('../middlewares/auth');
 const userRouter = require('./users');
 const movieRouter = require('./movies');
 const NotFoundError = require('../errors/not-found-error');
 
-// router.post('/signup', validateAuthInfo, createUser);
-// router.post('/signin', validateAuthInfo, login);
-// router.use(auth);
+router.post('/signup', /* validateAuthInfo, */ createUser);
+router.post('/signin', /* validateAuthInfo, */ login);
+router.use(auth);
 router.use('/users', userRouter);
 router.use('/movies', movieRouter);
 router.use((_req, _res, next) => {
