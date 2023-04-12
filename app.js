0347644019cebad825701d00b0db70c0c44500b6
Vladimir Frolov <frolov_vo@inbox.ru>
2023-04-12 22:02:11 +0300

    feat: add routes to app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
+
 const { PORT, DB_ADDRESS, ORIGIN } = require('./config');
 
 const app = express();
+app.use(routes);
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_ADDRESS);
