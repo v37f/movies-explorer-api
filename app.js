b04050fc712792a388c3f608411c6d89be90ac22
Vladimir Frolov <frolov_vo@inbox.ru>
2023-04-13 23:37:16 +0300

    feat: add logger

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { errorHandler } = require('./middlewares/error-handler');
 const { PORT, DB_ADDRESS, ORIGIN } = require('./config');
 
 const app = express();
 app.use(express.json());
+app.use(requestLogger);
 app.use(routes);
+app.use(errorLogger);
 app.use(errorHandler);
 
 mongoose.set('strictQuery', true);
