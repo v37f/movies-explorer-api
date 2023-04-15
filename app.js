74bf7c2dc89c74e22521539d2f70187cd7fd3ebd
Vladimir Frolov <frolov_vo@inbox.ru>
2023-04-15 22:35:40 +0300

    feat: add helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const routes = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -8,6 +9,7 @@ const { errorHandler } = require('./middlewares/error-handler');
 const { PORT, DB_ADDRESS, ORIGIN } = require('./config');
 
 const app = express();
+app.use(helmet());
 app.use(cors({ origin: ORIGIN }));
 app.use(express.json());
 app.use(requestLogger);
