e2642c247e2b40116c6d63edb36003461a9bfabd
Vladimir Frolov <frolov_vo@inbox.ru>
2023-06-18 20:43:46 +0300

    refactor: change ports

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 const { NODE_ENV } = process.env;
 const { JWT_SECRET_PRODUCTION } = process.env;
-const { PORT = '3000' } = process.env;
+const { PORT = '3001' } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
-const { ORIGIN = 'localhost:3001' } = process.env;
+const { ORIGIN = 'localhost:3000' } = process.env;
 
 const JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET_PRODUCTION : 'dev-secret';
 
